aa90fdf3d03d16f25519a915708685308fe18534
smallbizsolutions <austinrnorthrop@gmail.com>
2025-10-23 20:59:56 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import uploadRouter from "./routes/upload.js";
+import chatRouter from "./routes/chat.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/upload", uploadRouter);
+app.use("/api/chat", chatRouter);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
